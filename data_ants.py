741dbb313f53d3f9ae88cea389c3b58da36a6020
Shangda SD1 Yang <yangsd1@Lenovo.com>
2018-09-04 16:35:21 +0800

    update history

---
+++ data_ants.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import argparse
+import datetime
+import fnmatch
+import pluggy
+from pymongo import MongoClient
+import configparser
+from migration.hookspec import ParserSpec, ExcelBasicPlugin, ExcelReadmePlugin
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+PLUGIN = {
+    'ExcelBasicPlugin': ExcelBasicPlugin,
+    'ExcelReadmePlugin': ExcelReadmePlugin
+}
+
+
+def load_source(path):
+    assert path
+    if not os.path.isabs(path):
+        path = os.path.abspath(path)
+
+    def _get_all_files(path):
+        if os.path.isdir(path):
+            for root, dirs, files in os.walk(path):
+                for file in files:
+                    yield os.path.join(root, file)
+        else:
+            yield path
+
+    yield _get_all_files(path)
+
+
+def import_to_db(path, hook, config):
+    ipaddr = args.conf['DEFAULT']['mongo_ip_addr']
+    port = args.conf['DEFAULT']['mongo_port']
+    db = args.conf['DEFAULT']['data_set']
+
+    client = MongoClient(ipaddr, int(port))
+    if path.endswith(config.filter):
+        mongo_db = client[db]
+        mongo_collection = mongo_db[args.project]
+        mongo_collection.insert_one({"filename": path})
+    if path.endswith(config.index):
+        hook.get_indexes(path=path, config=config)
+        hook.parser(path=path, config=config)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Migrate test data source to database')
+    parser.add_argument('-c', '--conf', nargs='?',
+                        help="general configuration",
+                        default=config
+                        )
+    parser.add_argument('-d', '--directory', nargs='+',
+                        help='file system directory store original test data for a project',
+                        default=[]
+                        )
+    parser.add_argument('-f', '--filter', nargs='+',
+                        help='test resource type to be filter out and to be import to mongodb',
+                        default=('mp3',)
+                        )
+    parser.add_argument('-i', '--index', nargs='+',
+                        help='test resource index file type, such as excel',
+                        default=('xlsx', 'xls')
+                        )
+    parser.add_argument('-t', '--title', nargs='?',
+                        type=int,
+                        help='test resource index file type, such as excel',
+                        default=0
+                        )
+    parser.add_argument('-s', '--sheet', nargs='?',
+                        type=str,
+                        help='if index file is excel, specific sheet name',
+                        default='TestCase'
+                        )
+    parser.add_argument("--debug",
+                        action="store_true",
+                        help="enable debugging with maintainer email"
+                        )
+    args = parser.parse_args()
+
+    pm = pluggy.PluginManager("index-parser")
+    pm.add_hookspecs(ParserSpec)
+    pm.register(PLUGIN.get(args.conf['DEFAULT']['plugin_name'])())
+
+    for path in args.directory:
+        items = load_source(path)
+        for item in items:
+            for value in item:
+                import_to_db(value, pm.hook, args)
+
+
---
+++ hookspec2.py
@@ -0,0 +1,193 @@
+import os
+import re
+import xlrd
+import csv
+import pluggy
+import tempfile
+
+
+spec = pluggy.HookspecMarker("index-parser")
+impl = pluggy.HookimplMarker("index-parser")
+
+
+def replace_suffix(path, index_type, expect):
+    rep = dict(zip(index_type, [expect]*len(index_type)))
+    rep = dict((re.escape(k), v) for k, v in rep.items())
+    pattern = re.compile("|".join(rep.keys()))
+    return pattern.sub(lambda m: rep[re.escape(m.group(0))], path)
+
+
+class ParserSpec:
+
+    @spec
+    def get_indexes(self, path, config):
+        """get indexes
+        """
+
+    @spec
+    def parser(self, path, config):
+        """parser a path with index type
+        """
+
+
+class ExcelBasicPlugin:
+
+    @impl
+    def parser(self, path, config):
+        basename = os.path.basename(path).split('.')[0]
+
+        def _convert_to_csv(path, index_type, sheet, title_line):
+            wb = xlrd.open_workbook(path)
+            try:
+                sh = wb.sheet_by_name(sheet)
+                print(sh.row_values(title_line))
+                fileds = sh.row_values(title_line)
+                out_path = replace_suffix(path, index_type, 'csv')
+
+                with open(out_path, 'w', encoding='utf-8') as dest:
+                    wr = csv.writer(dest, quoting=csv.QUOTE_ALL)
+                    for row_num in range(title_line, sh.nrows):
+                        wr.writerow(sh.row_values(row_num))
+                return out_path, fileds
+            except xlrd.biffh.XLRDError:
+                print("No sheet {} in file {}".format(sheet, path))
+
+        def _upload_csv(path, fileds, config):
+            import subprocess
+            ipaddr = config.conf['DEFAULT']['mongo_ip_addr']
+            port = config.conf['DEFAULT']['mongo_port']
+            db = config.conf['DEFAULT']['data_set']
+            collection = basename + '_' + config.sheet
+
+            # delete db before update new csv file
+            subprocess.run(["mongo",
+                            "--host",
+                            ipaddr,
+                            "--port",
+                            port,
+                            db,
+                            "--eval",
+                            "db.dropDatabase()"
+                            ])
+
+            subprocess.run(["mongoimport",
+                            "--host",
+                            ipaddr,
+                            "--port",
+                            port,
+                            "--db",
+                            db,
+                            "--collection",
+                            collection,
+                            "--type",
+                            "csv",
+                            # "--mode",
+                            # "upsert",
+                            # "--upsertFields",
+                            # ','.join(fileds),
+                            "--headerline",
+                            "--file",
+                            path
+                            ])
+
+        sheets = config.sheet or config.conf['SHEET'].get(basename).split(',')
+        title_line = config.conf['TITLE'].get(basename)
+        for sheet in sheets:
+            try:
+                path, fields = _convert_to_csv(path, config.index, sheet, int(title_line))
+                _upload_csv(path, fields, config)
+            except TypeError:
+                pass
+
+
+class ExcelReadmePlugin:
+
+    def __init__(self):
+        self.indexes = []
+
+    @spec
+    def get_indexes(self, path, config):
+        """get indexes
+        """
+        index_type, sheet = config.index, config.sheet
+
+        wb = xlrd.open_workbook(path)
+        sh = wb.sheet_by_name(sheet)
+        out_path = replace_suffix(path, index_type, 'csv')
+        with open(out_path, 'w', encoding='utf-8') as dest_csv:
+            wr = csv.writer(dest_csv, quoting=csv.QUOTE_ALL)
+            for row_num in range(2, 13):
+                wr.writerow(sh.row_values(row_num))
+                self.indexes.append(sh.row_values(row_num))
+
+    @impl
+    def parser(self, path, config):
+        index_type, sheet, title = config.index, config.sheet, config.title
+
+        def _convert_to_csv(path, sheet, title):
+            wb = xlrd.open_workbook(path)
+            sh = wb.sheet_by_name(sheet)
+            print(sh.row_values(title))
+            fileds = sh.row_values(title)
+            out_path = replace_suffix(path, index_type, 'csv')
+            with open(out_path, 'w', encoding='utf-8') as dest_csv:
+                wr = csv.writer(dest_csv, quoting=csv.QUOTE_ALL)
+                for row_num in range(title, sh.nrows):
+                    wr.writerow(sh.row_values(row_num))
+            return out_path, fileds
+
+        def _upload_csv(path, fileds, config):
+            import subprocess
+            basename = os.path.basename(path).split('.')[0]
+            ipaddr = config.conf['DEFAULT']['mongo_ip_addr']
+            port = config.conf['DEFAULT']['mongo_port']
+            db = config.conf['DEFAULT']['data_set']
+            collection = config.conf['MAPPER'].get(basename) or basename
+
+            # delete db before update new csv file
+            subprocess.run(["mongo",
+                            "--host",
+                            ipaddr,
+                            "--port",
+                            port,
+                            db,
+                            "--eval",
+                            "db.dropDatabase()"
+                            ])
+
+            subprocess.run(["mongoimport",
+                            "--host",
+                            ipaddr,
+                            "--port",
+                            port,
+                            "--db",
+                            db,
+                            "--collection",
+                            collection,
+                            "--type",
+                            "csv",
+                            # "--mode",
+                            # "upsert",
+                            # "--upsertFields",
+                            # ','.join(fileds),
+                            "--headerline",
+                            "--file",
+                            path
+                            ])
+
+        for indexed_path in self.indexes:
+            # to be doing
+            basename, full_path = indexed_path[1], indexed_path[8]
+            path, fields = _convert_to_csv(full_path, sheet, title)
+            _upload_csv(path, fields, config)
+
+
+
+
+
+
+
+
+
+
+
